f090c5ddc061a772b245eae8237b4cdbf14bf027
itziar-mengual <itziar.mengual@gmail.com>
2024-11-15 22:20:24 +0100

    Update 15/11/24 It still doesn't work, we are encountering an error.

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 # main.py
-from config import board, pins, calibrate_shutter
-from utils import sensor_select, sensor_acquire_mean
-import time
+from process import setup_pins, clean_process, sample_process, board
+from config import CLEANING_DURATION, SAMPLING_DURATION
 
 if __name__ == "__main__":
-    calibrate_shutter()
-    
-    # Simple connection check to test air valve, pump, and sample valve
     try:
-        print("Testing air valve and pump...")
-        pins['air_valve'].write(1)
-        pins['pump'].write(1)
-        time.sleep(10)
-        pins['pump'].write(0)
-        pins['air_valve'].write(0)
-        time.sleep(5)
-        
-        print("Testing sample valve and pump...")
-        pins['sample_valve'].write(1)
-        pins['pump'].write(1)
-        time.sleep(10)
-        pins['pump'].write(0)
-        pins['sample_valve'].write(0)
-    
-    except KeyboardInterrupt:
-        print("Process interrupted.")
-    
+        # Setup the Arduino pins
+        setup_pins()
+
+        # Perform cleaning process
+        clean_process(CLEANING_DURATION)
+
+        # Perform sampling process
+        sample_process(SAMPLING_DURATION)
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
     finally:
+        # Ensure the Arduino connection is properly closed
         board.exit()
-        print("Arduino connection closed.")
+        print("Disconnected from Arduino.")
